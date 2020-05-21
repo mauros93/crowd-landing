c23683cd5bbd35d2638b37bcd9c34aa4866ae6bd
Mauricio Builes <builes@utp.edu.co>
2020-05-21 15:49:17 -0500

    Hemos añadido el valor que el usuario debe pagar

--- js.js
+++ js.js
@@ -2,3 +2,20 @@ function prueba(val){
     document.getElementById("demo1").innerHTML = val
 }
 
+var dinero = 0;
+function prueba(val){
+    dinero = val
+    dinero2 = "$ " + new Intl.NumberFormat("de-DE").format(val);
+    document.getElementById("demo1").innerHTML = dinero2;
+    document.getElementById("valor").innerHTML = dinero2;
+    var intereses = dinero * 0.025;
+    var seguro = 2000 + dinero * 0.013;
+    var administracion = 3000 + dinero * 0.0102;
+    var total = intereses + seguro + administracion;    
+    document.getElementById("intereses").innerHTML = "$ " + new Intl.NumberFormat("de-DE").format(intereses);
+    document.getElementById("seguro").innerHTML = "$ " + new Intl.NumberFormat("de-DE").format(seguro);
+    document.getElementById("administracion").innerHTML = "$ " + new Intl.NumberFormat("de-DE").format(administracion);
+    document.getElementById("total").innerHTML = "$ " + new Intl.NumberFormat("de-DE").format(total);    
+}
+
+
